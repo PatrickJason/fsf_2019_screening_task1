d0a96216cc86840c42179f7d92496c010b0f43df
PatrickJason <jpatrickjason@gmail.com>
2019-03-31 21:57:31 +0530

    Documentation edits

--- teams/views.py
+++ teams/views.py
@@ -21,27 +21,31 @@ class CreateTeams(LoginRequiredMixin, generic.CreateView):
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
-        # return super().get(request, *args, **kwargs)
         return super(CreateTeams, self).form_valid(form)
 class SingleTeams(generic.DetailView):
     model = Teams
     def get_context_data(self, **kwargs):
         teams1=[]
         for t in Teams.objects.all():
-        teams1.append([team for team in Teams.objects.all() if self.request.user in t.team_members.all()])
+            teams1.append([team for team in Teams.objects.all() if self.request.user in t.team_members.all()])
+        # Store the teams created by the user
         teams_created = [team for team in Teams.objects.all() if self.request.user == team.created_by]
+        # Store the tasks created by the user
         tasks_created = [task for task in Tasks.objects.all() if self.request.user == task.task_creator]
+        # Store the tasks assigned to the user
         tasks_assigned = [task for task in Tasks.objects.all() if self.request.user == task.assignee]
         print(teams_created)
         team_str=[]
         for t in teams_created:
             team_str.append(str(t))
         print(team_str)
+        # Store the tasks in the teams in which the user is a member
         my_team_tasks = []
         for team in teams1:
             for task in Tasks.objects.all():
                 if task.assigned_to_team == team:
                     my_team_tasks.append(task)
+        # Adding additional context to be used by the views
         context = super(SingleTeams, self).get_context_data(**kwargs)
         context['teams1'] = teams1
         context['teams_created']=teams_created
